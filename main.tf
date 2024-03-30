db485c0133d857cb810bd566b6c45b3ca4ea71c1
Pankaj Narang <pnarang@Pankajs-MacBook-Air.local>
2024-03-30 18:20:19 -0400

    adding azuread & random provider for AKS implementation

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "~> 2.47.0"
+    }
+    random  = {
+      source = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
   }
   required_version = ">= 1.1.0"
 }
@@ -12,6 +20,7 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_pet" "random" {}
 resource "azurerm_resource_group" "pankajnarang" {
   name     = var.resource_group_name
   location = var.location
